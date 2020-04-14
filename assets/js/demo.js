248858244e35fb6b642096e0b1d55d52a41d117a
tabichan01 <184449F@mymail.nyp.edu.sg>
2020-04-14 18:42:37 +0800

    first commit

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,5 +1,5 @@
 "use strict";
-// Cicle Chart
+// Cicle Chart - REAL
 Circles.create({
 	id:           'task-complete',
 	radius:       50,
@@ -43,7 +43,7 @@ $('#map-example').vectorMap(
 	normalizeFunction: 'linear',
 	scaleColors: ['#b6d6ff', '#005ace'],
 	selectedColor: '#35cd3a',
-	selectedRegions: ['ID', 'RU', 'US', 'AU', 'CN', 'BR'],
+	selectedRegions: ['ID', 'RU', 'US', 'AU', 'CN', 'BR', ],
 	showTooltip: true,
 	onRegionClick: function(element, code, region)
 	{
@@ -95,7 +95,7 @@ var statisticsChart = new Chart(ctx, {
 		}]
 	},
 	options : {
-		responsive: true, 
+		responsive: true,
 		maintainAspectRatio: false,
 		legend: {
 			display: false
@@ -136,20 +136,20 @@ var statisticsChart = new Chart(ctx, {
 					fontStyle: "500"
 				}
 			}]
-		}, 
-		legendCallback: function(chart) { 
-			var text = []; 
-			text.push('<ul class="' + chart.id + '-legend html-legend">'); 
-			for (var i = 0; i < chart.data.datasets.length; i++) { 
-				text.push('<li><span style="background-color:' + chart.data.datasets[i].legendColor + '"></span>'); 
-				if (chart.data.datasets[i].label) { 
-					text.push(chart.data.datasets[i].label); 
-				} 
-				text.push('</li>'); 
-			} 
-			text.push('</ul>'); 
-			return text.join(''); 
-		}  
+		},
+		legendCallback: function(chart) {
+			var text = [];
+			text.push('<ul class="' + chart.id + '-legend html-legend">');
+			for (var i = 0; i < chart.data.datasets.length; i++) {
+				text.push('<li><span style="background-color:' + chart.data.datasets[i].legendColor + '"></span>');
+				if (chart.data.datasets[i].label) {
+					text.push(chart.data.datasets[i].label);
+				}
+				text.push('</li>');
+			}
+			text.push('</ul>');
+			return text.join('');
+		}
 	}
 });
 
@@ -163,61 +163,6 @@ var legendItems = myLegendContainer.getElementsByTagName('li');
 for (var i = 0; i < legendItems.length; i += 1) {
 	legendItems[i].addEventListener("click", legendClickCallback, false);
 }
-
-var dailySalesChart = document.getElementById('dailySalesChart').getContext('2d');
-
-var myDailySalesChart = new Chart(dailySalesChart, {
-	type: 'line',
-	data: {
-		labels:["January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September"],
-		datasets:[ {
-			label: "Sales Analytics", fill: !0, backgroundColor: "rgba(255,255,255,0.2)", borderColor: "#fff", borderCapStyle: "butt", borderDash: [], borderDashOffset: 0, pointBorderColor: "#fff", pointBackgroundColor: "#fff", pointBorderWidth: 1, pointHoverRadius: 5, pointHoverBackgroundColor: "#fff", pointHoverBorderColor: "#fff", pointHoverBorderWidth: 1, pointRadius: 1, pointHitRadius: 5, data: [65, 59, 80, 81, 56, 55, 40, 35, 30]
-		}]
-	},
-	options : {
-		maintainAspectRatio:!1, legend: {
-			display: !1
-		}
-		, animation: {
-			easing: "easeInOutBack"
-		}
-		, scales: {
-			yAxes:[ {
-				display:!1, ticks: {
-					fontColor: "rgba(0,0,0,0.5)", fontStyle: "bold", beginAtZero: !0, maxTicksLimit: 10, padding: 0
-				}
-				, gridLines: {
-					drawTicks: !1, display: !1
-				}
-			}
-			], xAxes:[ {
-				display:!1, gridLines: {
-					zeroLineColor: "transparent"
-				}
-				, ticks: {
-					padding: -20, fontColor: "rgba(255,255,255,0.2)", fontStyle: "bold"
-				}
-			}
-			]
-		}
-	}
-});
-
-$("#activeUsersChart").sparkline([112,109,120,107,110,85,87,90,102,109,120,99,110,85,87,94], {
-	type: 'bar',
-	height: '100',
-	barWidth: 9,
-	barSpacing: 10,
-	barColor: 'rgba(255,255,255,.3)'
-});
 
 var topProductsChart = document.getElementById('topProductsChart').getContext('2d');
 
